c07d7b7aadaacb9262b0d63cd468151dfcd29e32
Philip Huang <p208p2002@gmail.com>
2023-03-24 13:04:54 +0800

    feat: update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
-
-cd /tmp
-_ARCH=$(uname -m)
-
-if [[ "$OSTYPE" == "linux-gnu"* && "$_ARCH" == "x86_64" ]]; then 
-    wget https://github.com/p208p2002/heygpt/releases/download/latest-Linux-X64/heygpt-Linux-X64.tar.gz
-    tar -xzf heygpt-Linux-X64.tar.gz
-elif [[ "$OSTYPE" == "darwin"* && "$_ARCH" == "x86_64"  ]]; then
-    wget https://github.com/p208p2002/heygpt/releases/download/latest-macOS-X64/heygpt-macOS-X64.tar.gz
-    tar -xzf heygpt-macOS-X64.tar.gz
-elif [[ "$OSTYPE" == "darwin"* && "$_ARCH" == "arm64"  ]]; then
-    wget https://github.com/p208p2002/heygpt/releases/download/latest-macOS-ARM64/heygpt-macOS-ARM64.tar.gz
-    tar -xzf heygpt-macOS-ARM64.tar.gz
-else
-    echo "Heygpt not available for this computer"
+# Check CMD exist
+if ! [ -x "$(command -v tar)" ]; then
+  echo 'Error: tar is not installed.' >&2
+  exit 1
 fi
 
+if ! [ -x "$(command -v curl)" ]; then
+  echo 'Error: curl is not installed.' >&2
+  exit 1
+fi
 
-cp target/release/heygpt /usr/local/bin
+# 
+rm -rf /tmp/heygpt
 
-rm -rf /tmp/target
+#
+cd /tmp
+_ARCH=$(uname -m)
+_ID=$(id -u)
+
+#
+if [[ "$OSTYPE" == "linux-gnu"* && "$_ARCH" == "x86_64" ]]; then 
+    # corrected URL
+    curl -o heygpt.tar.gz -LJ https://github.com/p208p2002/heygpt/releases/download/latest-Linux-X64/heygpt-Linux-X64.tar.gz
+    
+elif [[ "$OSTYPE" == "darwin"* && "$_ARCH" == "x86_64"  ]]; then
+    # corrected URL
+    curl -o heygpt.tar.gz -LJ https://github.com/p208p2002/heygpt/releases/download/latest-macOS-X64/heygpt-macOS-X64.tar.gz
+    
+elif [[ "$OSTYPE" == "darwin"* && "$_ARCH" == "arm64"  ]]; then
+    # corrected URL
+    curl -o heygpt.tar.gz -LJ https://github.com/p208p2002/heygpt/releases/download/latest-macOS-ARM64/heygpt-macOS-ARM64.tar.gz
+else
+    echo "Heygpt not available for this computer"
+    exit 1
+fi
+
+tar -xzf heygpt.tar.gz
+
+if [[ "$_ID" == 0 ]]; then 
+    cp target/release/heygpt /usr/local/bin 
+else
+    sudo cp target/release/heygpt /usr/local/bin
+fi
+
+rm -rf /tmp/heygpt.tar.gz
+rm -rf /tmp/heygpt
